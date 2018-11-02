cecdacae202c46f47e64923ac33d802c9759e5fc
Lena <karengvcs@gmail.com>
2018-11-01 20:11:16 -0500

    Controles y opciones de mause para graficar

--- trabajo-unidad2/opengl1.cpp
+++ trabajo-unidad2/opengl1.cpp
@@ -2,33 +2,134 @@
 #include <stdlib.h>
 #include <cmath>
 #include <iostream>
+#include <vector>
 using namespace std;
 //g++ turtle.cpp -o g1 -lglut -lGLU -lGL^C compilacion
-void init(void){
-	glClearColor(1.0,1.0,1.0,1.0); //asigna el color  de la ventana(blanco)
-	glClear(GL_COLOR_BUFFER_BIT);// establece parametross de proyeccion
-	gluOrtho2D(0.0,200.0,0.0,150.0);
-	glPointSize(5);
-}
-//int round(const double a){ return (int)(a+0.5);}
+bool check = true;
+int x,y,xf,yf;
+int fg_menu,color_menu,numberOfLists=0, numberOfPoints=0;
+vector<int> points;
 
-void setPixel(int x, int y)
-{	
+
+void mainMenu (int id){
+    double lineWidth, color[4];
+
+    switch (id)
+    {
+    case 1 : /* reset default values */
+        glNewList (++numberOfLists, GL_COMPILE_AND_EXECUTE);
+           glColor3d (0.0, 0.0, 0.0);
+           glLineWidth (1.0);
+        glEndList ();
+        break;
+    case 2 : /* clear the screen */
+        glClearColor(1.0,1.0,1.0,1.0);
+    	glClear(GL_COLOR_BUFFER_BIT);
+    	check = false;
+        break;
+    case 3 : /* exit the program */
+        exit (0);
+        break;
+    default : /* in case none of the above occur */
+        break;
+    }
+}
+
+void colorMenu (int id){
+    glNewList (++numberOfLists, GL_COMPILE_AND_EXECUTE);
+    switch (id)
+    {
+    case 1 : /* change color to red */
+        glColor3d (1.0, 0.0, 0.0);
+        break;
+    case 2 : /* change color to green */
+        glColor3d (0.0, 1.0, 0.0);
+        break;
+    case 3 : /* change color to blue */
+        glColor3d (0.0, 0.0, 1.0);
+        break;
+    case 4 : /* change color to black */
+        glColor3d (0.0, 0.0, 0.0);
+        break;
+    default : /* for any case not covered above, leave color unchanged */
+        break;
+    }
+    glEndList ();
+    
+}
+
+void figuraMenu(int id)
+/* This is the callback function for the size menu. */
+{
+    glNewList (++numberOfLists, GL_COMPILE_AND_EXECUTE);
+    switch (id)
+    {
+    case 1 : /* change line thickness to 1 */
+        
+        break;
+    case 2 : /* change line thickness to 2 */
+        
+        break;
+    case 3 : /* change line thickness to 3 */
+        
+        break;
+    default : /* for any case not covered above, leave line thickness unchanged */
+        break;
+    }
+    glEndList ();
+}
+
+void init(void){
+	glClearColor(1,1,1,1); //asigna el color  de la ventana(blanco)
+	glClear(GL_COLOR_BUFFER_BIT);// establece parametross de proyeccion
+	glColor3f(1,0,0);
+	glPointSize(3);
+	gluOrtho2D(0.0,600.0,0.0,500.0);
+
+	/* Create the menu structure and attach it to the right mouse button. */
+
+    color_menu = glutCreateMenu (colorMenu);
+       glutAddMenuEntry ("Rojo", 1);
+       glutAddMenuEntry ("Verde", 2);
+       glutAddMenuEntry ("Azul", 3);
+       glutAddMenuEntry ("Negro", 4);
+    fg_menu = glutCreateMenu (figuraMenu);
+       glutAddMenuEntry ("Poligono", 1);
+       glutAddMenuEntry ("Linea", 2);
+       glutAddMenuEntry ("Circulo", 3);
+       glutAddMenuEntry ("Parabola", 3);
+    glutCreateMenu (mainMenu);
+       glutAddSubMenu ("Color", color_menu);
+       glutAddSubMenu ("Figura", fg_menu);
+       glutAddMenuEntry ("Limpiar ventana", 2);
+       glutAddMenuEntry ("Salir", 3);
+    glutAttachMenu (GLUT_RIGHT_BUTTON);
+}
+
+
+void setPixel(int x_, int y_){	
+	if(numberOfPoints == 0){
+		x = x_; y = y_;
+	}else
+		xf = x_; yf = y_;
+	numberOfPoints++;
     glBegin(GL_POINTS);
-    glVertex2i(x, y); //Set pixel coordinates 
+    glVertex2i(x_, y_); //Set pixel coordinates 
     glEnd();
     glFlush(); //Render pixel
 }
-void ControlMause(int button, int state, int x, int y) {
-	glColor3f(1.0,0.0,0.0);
-	glPointSize(5);
+void ControlMause(int button, int state, int xmouse, int ymouse) {
 	cout<<x<<" ; "<<y<<endl;
-    if (button==GLUT_LEFT_BUTTON && state==GLUT_UP){
-    	setPixel(x, y);
+    if (button==GLUT_LEFT_BUTTON && state==GLUT_DOWN){
+    	check = true;
+    	x = xmouse;
+    	y = 500 - ymouse;
+    }else{
+    	cout<<"dibuja"<<endl;
     }
-    
     glutPostRedisplay();
 }
+
 void lineDDA(int xi, int yi, int xf, int yf){
 	int dx = xf - xi, dy = yf - yi,steps;
 	double incX, incY, x = xi, y = yi;
@@ -45,34 +146,32 @@ void lineDDA(int xi, int yi, int xf, int yf){
 
 
 void funcion1(void){
-	glColor3f(1.0,0.0,0.0);//color del segmento(linea roja)
-	/*lineDDA(2,1,100,70);
-	glColor3f(1.0,1.0,0.0);//color del segmento(linea roja)
+	int pos;
+	if(check){
+		setPixel(x,y);
+	}
+	if(numberOfPoints == 2){
+		lineDDA(x,y,xf,yf);
+		numberOfPoints = 0;
+	}
+	//lineDDA(2,1,10,100);
+	/*glColor3f(1.0,1.0,0.0);//color del segmento(linea roja)
 	lineDDA(2,1,100,200);
 	glColor3f(1.0,0.0,1.0);//color del segmento(linea roja)
 	lineDDA(1,100,200,2);*/
-	glutMouseFunc(ControlMause);
+	//glutMouseFunc(ControlMause);
 }
-void name_funcion(void){
-	glClear(GL_COLOR_BUFFER_BIT); // borra ventana de visualizcion
-	glColor3f(1.0,0.0,0.0);//color del segmento(linea roja)
-	glPointSize(5);
-	glBegin(GL_POINTS);
-		glVertex2i (50, 100);
-		glVertex2i (75, 50);
-		glVertex2i (100, 20);
-	glEnd();
-	glFlush(); //procesa lassubrutinas lo mas rapido posible
-}
+
 int main(int argc, char  **argv)
 {
 	glutInit(&argc,argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
-	glutInitWindowPosition(50,100);
+	glutInitWindowPosition(50,50);
 	glutInitWindowSize(600,500);
 	glutCreateWindow("DIBUJAR#1");
 
 	init();
 	glutDisplayFunc(funcion1); //name_funcion-> funcion a correr
+	glutMouseFunc(ControlMause);
 	glutMainLoop();
 }
